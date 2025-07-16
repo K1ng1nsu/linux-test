ab5460d63ee18c9440221bd5f57a227df5bbb216
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-07-16 15:17:26 +0900

    real last commit

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String hello(){
-        return "Hello World";
+    public String hello(HttpServletRequest request) {
+
+        return "Hello World" + request.getRemoteAddr();
     }
 }
