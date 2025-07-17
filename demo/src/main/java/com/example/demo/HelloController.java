526952af8965ba7f3014db63c11a436ff837deb6
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-07-17 11:08:34 +0900

    real real real last commit

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HelloController {
 
     private final GreetingRepository greetingRepository;
